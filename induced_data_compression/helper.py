708d614e40940dde6a2bfb5a41502f551013d280
matthiasGmayer <matthias.georg.mayer@gmail.com>
2022-07-09 19:24:14 +0200

    add helper.py
    forgot to add that in the last commit.

---
+++ induced_data_compression/helper.py
@@ -0,0 +1,11 @@
+def unique(sequence, key=id):
+    """
+    :param sequence: the sequence to be filtered
+    :param key: a function of the elements of the sequence. If the key is the same the elements are regarded as the same
+    :return: yields the unique elements of the sequence (as a generator)
+    """
+    d = set()
+    for s in sequence:
+        if key(s) not in d:
+            d.add(key(s))
+            yield s
